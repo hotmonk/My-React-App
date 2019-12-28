a89d0f05a9da674812f7b0da1b1ef07d78b7c046
Hotmonk <avinashkhetri@gmail.com>
2019-12-28 18:58:31 +0530

    Toggle data button added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name:'Anubhav', age:19},
       {name:'Avinash', age:20},
       {name:'DevJ', age:18},
-    ]
+    ],
+    showPerson:false
   }
 
 ageChangedHandler=(event=>{
@@ -22,8 +23,6 @@ ageChangedHandler=(event=>{
 })
 
   switchNameHandler=(newName)=>{
-    // console.log('was Clicked') 
-    // DONT DO THIS this.state.persons[0].name="Anubhav Mittal";
     this.setState({
       persons:[
         {name:newName, age:19},
@@ -34,6 +33,10 @@ ageChangedHandler=(event=>{
   }
 // HOOKS change value
 
+togglePersonHandler=()=>{
+  const doesShow=this.state.showPerson;
+  this.setState({showPerson: !doesShow});
+}
   render() {
     const style={
       backgroundColor: 'white',
@@ -42,31 +45,37 @@ ageChangedHandler=(event=>{
       padding: '8px',
       cursor: 'pointer'
     }
+
+    let personsexist=null;
+
+    if(this.state.showPerson){
+      personsexist=(
+        <div>
+          <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>                          
+          <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,"Tatya Vichu")}                     // to pass the variable we can use bind or we an use what we did above with button onclick
+          changed={this.ageChangedHandler}>show me da way</Person> 
+          <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>  
+        </div>
+      );
+    }
+
+
     return (                                //this is jsx not html so class word cannot be used
       <div className="App">                 
         <h1>Helllo world</h1>  
         <p>does this works?Should be a paragrapgh</p>  
-        <button style={style} onClick={()=>this.switchNameHandler("Anubhav with arraw func")}>Switch Name</button>
-        {/* we can also activate a function like we did here, but this is considered to be inefficient               */}
-        {/****IMP**** dont use paranthesis on switchNameHandler bcs otherwise it will activate it */}
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>                          
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this,"Tatya Vichu")}                     // to pass the variable we can use bind or we an use what we did above with button onclick
-        changed={this.ageChangedHandler}>show me da way</Person> 
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/> 
+        <button style={style} onClick={this.togglePersonHandler}>Switch Name</button>
+  
+        {personsexist}
       </div>
-      // <h1>can be added?</h1>             cant be done bcs oly one element in app
     );
-
-    //return React.createElement('div',null,'h1','Hiw adiwns');    wont work this way
-    //return React.createElement('div',null,React.createElement('h1',{className:'App'},'This should be in bigger size'))
-    // this will work but it willl be cumbersome to do so that is why we use above class name thingy
   }
 }
 
